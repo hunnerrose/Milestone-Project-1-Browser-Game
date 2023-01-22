be26a1e3c059d215c2f2e894f4c76212263ed7b5
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-01-22 14:18:39 -0800

    added BG, title, music and start menu

--- game.js
+++ game.js
@@ -3,7 +3,23 @@ const optionButtonsElement = document.getElementById("option-buttons")
 
 let state = {}
 
-function startGame(){
+let startLink = document.getElementById("start-link")
+
+function startMenu(){
+    document.querySelector('.container').style.display = 'none';
+    document.getElementById("start").style.display = 'block';
+    document.querySelector('#start-link').style.display= 'block';
+}
+
+startLink.onclick = function() {
+    document.querySelector('.container').style.display = 'block';
+    document.querySelector('#start-link').style.display= 'none';
+    document.getElementById("start").style.display = 'none';
+    playGame()
+}
+
+function playGame() {
+    document.querySelector('.container').style.display = 'block';
     state = {}
     showTextNode(1)
 }
@@ -13,7 +29,7 @@ function showTextNode(textNodeIndex){
     const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
     textElement.innerText = textNode.text
     while (optionButtonsElement.firstChild){
-        optionButtonsElement.removeChild(optionsButtonElement.firstChild)
+        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
 
     textNode.options.forEach(option => {
@@ -28,33 +44,169 @@ function showTextNode(textNodeIndex){
 }
 
 function showOption(option){
-    return true
+    return option.requiredState == null || option.nextText
 }
 
 function selectOption(option){
-
+    const nextTextNodeId = option.nextText
+    if (nextTextNodeId <= 0){
+        return startMenu()
+    }
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeId)
 }
 
 const textNodes = [
     {
-        id: 1,
-        text: "You're working one boring Tuesday morning at 7-Eleven, ringing up items, trying to make it through the rest of your shift whilst being violently hungover. You then work your way to the cooler in the back of the store to 'restock cold drinks' & catch a break from the bright lights & irritating social interactions leaving his coworker Stefon to checkout customers & fend for himself. You overhear a customer walk in stumbling, inaudible. Stefon asks them 'Are you okay' to which the customer growls & lunges for Stefon & bites his arm. What do you do?",
-        options: [
-            {
-                text: "Help Stefon",
-                setState: {helpStefon: true},
-                nextText: 2
-            },
-            {
-                text: "Stay hidden"
-                nextText: 2,
-            }
-        ]
+      id: 1,
+      text: "'You wake up in a strange place and you see a jar of blue goo near you.'",
+      options: [
+        {
+          text: 'Take the goo',
+          setState: { blueGoo: true },
+          nextText: 2
+        },
+        {
+          text: 'Leave the goo',
+          nextText: 2
+        }
+      ]
     },
     {
-        id: 2,
-        text:
+      id: 2,
+      text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+      options: [
+        {
+          text: 'Trade the goo for a sword',
+          requiredState: (currentState) => currentState.blueGoo,
+          setState: { blueGoo: false, sword: true },
+          nextText: 3
+        },
+        {
+          text: 'Trade the goo for a shield',
+          requiredState: (currentState) => currentState.blueGoo,
+          setState: { blueGoo: false, shield: true },
+          nextText: 3
+        },
+        {
+          text: 'Ignore the merchant',
+          nextText: 3
+        }
+      ]
+    },
+    {
+      id: 3,
+      text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+      options: [
+        {
+          text: 'Explore the castle',
+          nextText: 4
+        },
+        {
+          text: 'Find a room to sleep at in the town',
+          nextText: 5
+        },
+        {
+          text: 'Find some hay in a stable to sleep in',
+          nextText: 6
+        }
+      ]
+    },
+    {
+      id: 4,
+      text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.',
+      options: [
+        {
+          text: 'Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 5,
+      text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+      options: [
+        {
+          text: 'Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 6,
+      text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+      options: [
+        {
+          text: 'Explore the castle',
+          nextText: 7
+        }
+      ]
+    },
+    {
+      id: 7,
+      text: 'While exploring the castle you come across a horrible monster in your path.',
+      options: [
+        {
+          text: 'Try to run',
+          nextText: 8
+        },
+        {
+          text: 'Attack it with your sword',
+          requiredState: (currentState) => currentState.sword,
+          nextText: 9
+        },
+        {
+          text: 'Hide behind your shield',
+          requiredState: (currentState) => currentState.shield,
+          nextText: 10
+        },
+        {
+          text: 'Throw the blue goo at it',
+          requiredState: (currentState) => currentState.blueGoo,
+          nextText: 11
+        }
+      ]
+    },
+    {
+      id: 8,
+      text: 'Your attempts to run are in vain and the monster easily catches.',
+      options: [
+        {
+          text: 'Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 9,
+      text: 'You foolishly thought this monster could be slain with a single sword.',
+      options: [
+        {
+          text: 'Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 10,
+      text: 'The monster laughed as you hid behind your shield and ate you.',
+      options: [
+        {
+          text: 'Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 11,
+      text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+      options: [
+        {
+          text: 'Congratulations. Play Again.',
+          nextText: -1
+        }
+      ]
     }
-]
-
-startGame()
+  ]
+  
+startMenu()
