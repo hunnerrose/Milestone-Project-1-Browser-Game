ce7c147963fc60fc427f2502de65f1e82f502e58
Hunter Rawal <hunnerrose@Hunters-MacBook-Air.local>
2023-01-24 20:25:09 -0800

    added click effects, and finalized text prompts

--- game.js
+++ game.js
@@ -59,62 +59,62 @@ function selectOption(option){
 const textNodes = [
     {
       id: 1,
-      text: "'You wake up in a strange place and you see a jar of blue goo near you.'",
+      text: "A deadly virus has leaked from the city's underground lab and breached the surface. 'Conveniently', you're trapped at the Auto Zone you work at, downtown, with no car. Chaos is wreaking havoc on the outside, crazies biting people, explosions erupting sporadically and crashes left & right. You go to lock the doors when a man starts banging on the glass begging you to let him in. What do you do?",
       options: [
         {
-          text: 'Take the goo',
-          setState: { blueGoo: true },
+          text: 'Let him in',
+          setState: { letIn: true },
           nextText: 2
         },
         {
-          text: 'Leave the goo',
-          nextText: 2
+          text: 'Lock him out',
+          nextText: 11
         }
       ]
     },
     {
       id: 2,
-      text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+      text: "To his relief, you let him in & immediately lock the doors just seconds before a crazy slams against the door with nothing but blind hunger in its glazed eyes. You each back away from the door to avoid drawing any further attention and hide. 'Thank you so much, I'm David. Idk what's going on out there but I need to get TF away from this bullshit, but my car just died and won't start. Can you help?' ",
       options: [
         {
-          text: 'Trade the goo for a sword',
-          requiredState: (currentState) => currentState.blueGoo,
-          setState: { blueGoo: false, sword: true },
+          text: 'Get a new car battery',
+          requiredState: (currentState) => currentState.letIn,
+          setState: { letIn: false, battery: true },
           nextText: 3
         },
         {
-          text: 'Trade the goo for a shield',
-          requiredState: (currentState) => currentState.blueGoo,
-          setState: { blueGoo: false, shield: true },
+          text: 'Get ultrasafe jump starter + cables',
+          requiredState: (currentState) => currentState.letIn,
+          setState: { letIn: false, jumpStart: true },
           nextText: 3
         },
         {
-          text: 'Ignore the merchant',
-          nextText: 3
+          text: 'Decline help',
+          nextText: 12
         }
       ]
     },
     {
       id: 3,
-      text: 'After leaving the merchant you start to feel tired and stumble upon a small town next to a dangerous looking castle.',
+      text: 'You & David both determine youll need to make it to his car in order to quickly fix it up, but first you need to clear the path and the crazies in it. What weapon do you use?',
       options: [
         {
-          text: 'Explore the castle',
+          text: 'A gun from the hidden safe',
           nextText: 4
         },
         {
-          text: 'Find a room to sleep at in the town',
+          text: 'A wrench',
           nextText: 5
         },
         {
-          text: 'Find some hay in a stable to sleep in',
+          text: 'A mallet',
           nextText: 6
         }
       ]
     },
     {
       id: 4,
-      text: 'You are so tired that you fall asleep while exploring the castle and are killed by some terrible monster in your sleep.',
+      text: 'Uh-oh, the crazy came storming towards David who fired his gun on it, but the noise drew 15 more killing you both.',
       options: [
         {
           text: 'Restart',
@@ -124,7 +124,7 @@ const textNodes = [
     },
     {
       id: 5,
-      text: 'Without any money to buy a room you break into the nearest inn and fall asleep. After a few hours of sleep the owner of the inn finds you and has the town guard lock you in a cell.',
+      text: 'Oh no, a crazy came storming towards David who swung on him with the wrench but it barely did any damage and David was taken down, with you shorly after.',
       options: [
         {
           text: 'Restart',
@@ -134,79 +134,87 @@ const textNodes = [
     },
     {
       id: 6,
-      text: 'You wake up well rested and full of energy ready to explore the nearby castle.',
+      text: 'The crazy stormed for David while you took off with your equipment, but David managed to take him down with the mallet you gave him.',
       options: [
         {
-          text: 'Explore the castle',
+          text: 'Fix car',
           nextText: 7
         }
       ]
     },
     {
       id: 7,
-      text: 'While exploring the castle you come across a horrible monster in your path.',
+      text: 'You make it to Davids car when another couple of crazies come from the other side of the building. David is tough but hes only one man so you need to work quick. What do you do?',
       options: [
         {
-          text: 'Try to run',
+          text: 'Run away',
           nextText: 8
         },
         {
-          text: 'Attack it with your sword',
-          requiredState: (currentState) => currentState.sword,
+          text: 'Replace the battery',
+          requiredState: (currentState) => currentState.battery,
           nextText: 9
         },
         {
-          text: 'Hide behind your shield',
-          requiredState: (currentState) => currentState.shield,
+          text: 'Jump start the car',
+          requiredState: (currentState) => currentState.jumpStart,
           nextText: 10
         },
-        {
-          text: 'Throw the blue goo at it',
-          requiredState: (currentState) => currentState.blueGoo,
-          nextText: 11
-        }
       ]
     },
     {
       id: 8,
-      text: 'Your attempts to run are in vain and the monster easily catches.',
+      text: 'You leave your so-called buddy David hanging for him to get eaten by the crazies and go hide in a dumpster. You wait it out like a coward until the city is nuked and you along with it.',
       options: [
         {
-          text: 'Restart',
+          text: 'Game Over: Restart',
           nextText: -1
         }
       ]
     },
     {
       id: 9,
-      text: 'You foolishly thought this monster could be slain with a single sword.',
+      text: 'You quickly replace the battery just to try turning on the car and find that one is dead too. (Your stupid co-worker Carl just had to be sneaky once again and trade his out for a new one for free.99.) Both you and David are torn to shreds.',
       options: [
         {
-          text: 'Restart',
+          text: 'Game Over: Restart',
           nextText: -1
         }
       ]
     },
     {
       id: 10,
-      text: 'The monster laughed as you hid behind your shield and ate you.',
+      text: 'You quickly jump start the car finally turning it on. David takes out the few crazies he could before jumping into the passenger seat. You take off to the mountains and never look back, finally escaping the city and the bomb that takes it out.',
       options: [
         {
-          text: 'Restart',
+          text: 'Congratulations! Play Again.',
           nextText: -1
         }
       ]
     },
     {
       id: 11,
-      text: 'You threw your jar of goo at the monster and it exploded. After the dust settled you saw the monster was destroyed. Seeing your victory you decide to claim this castle as your and live out the rest of your days there.',
+      text: 'Good job: you lock out your only chance at support; the man gets jumped by a bunch of other crazies who then notice you, break the store windows and attack you. Live a loner, die a loner.',
       options: [
         {
-          text: 'Congratulations. Play Again.',
+          text: 'Game Over: Restart',
+          nextText: -1
+        }
+      ]
+    },
+    {
+      id: 12,
+      text: 'Good job: you bail on your only chance at support. David grabs random car supplies, and jets out the backdoor leaving the crazies to come in and eat you alive. Live a loner, die a loner.',
+      options: [
+        {
+          text: 'Game Over: Restart',
           nextText: -1
         }
       ]
     }
   ]
+
+const audio = new Audio();
+audio.src = "assets/Gun Cocking Sound Effect.mp3" ;
   
 startMenu()
